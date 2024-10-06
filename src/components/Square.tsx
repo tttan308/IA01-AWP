85c9e5ec50c3f96dd20535050228365d125ac386
tantran <tantran.300803@gmail.com>
2024-10-06 19:26:17 +0700

    Refactor Square and Board components to use TypeScript interfaces

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,14 +1,14 @@
 interface SquareProps {
-    readonly value: string | null;
-    readonly onSquareClick: () => void;
+  readonly value: string | null;
+  readonly onSquareClick: () => void;
 }
 
 function Square({ value, onSquareClick }: SquareProps) {
-    return (
-        <button className="square" onClick={onSquareClick}>
-            {value}
-        </button>
-    );
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default Square;
